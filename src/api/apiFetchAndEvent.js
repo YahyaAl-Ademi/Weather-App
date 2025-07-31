afebd2976913758a1c9bd2fea6f9e06206945e06
Yahya Al-Ademi <yahyaalademi84@gmail.com>
2025-07-31 14:07:08 +0200

    Add .env.example and update API fetch logic

--- src/api/apiFetchAndEvent.js
+++ src/api/apiFetchAndEvent.js
@@ -3,7 +3,7 @@ import { saveInHistory } from '../storage/localStorage.js';
 import { renderWeather } from '../view/renderWeather.js';
 
 
-const baseURL = 'https://weather-app-ey6z.onrender.com';
+const baseURL = process.env.API_BASE_URL || 'http://localhost:3000';
 export async function fetchWeather(city) {
   const url = `${baseURL}/api/weather?city=${city}`;
 
